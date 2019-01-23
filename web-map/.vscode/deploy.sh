2cb3e0a00c8c14e97ff80cac3b0a4865eeab5a39
Simon Annetts <simonannetts@esdm.co.uk>
2019-01-23 14:46:06 +0000

    Make deploy.sh work on win and change prod settings

--- web-map/.vscode/deploy.sh
+++ web-map/.vscode/deploy.sh
@@ -5,12 +5,13 @@ host="esdm-xen1-04.esdm.co.uk"
 user="esdm"
 remotepath="/home/esdm/web-published"
 
-ng build --dist
+echo "Building solution in Production mode..."
+ng build --prod
 [ $? -ne 0 ] && echo "Build/Publish Task Failed!" && exit 1
 ssh $user@$host "cd $remotepath/$project && rm -Rf *"
 [ $? -ne 0 ] && echo "Deploy Clean Failed!" && exit 1
 sftp "$user@$host:$remotepath/$project" <<EOF
-mput -r dist/*
+mput -r dist/web-map/*
 EOF
 [ $? -ne 0 ] && echo "Deploy Transfer Failed!" && exit 1
 echo "Deploy Finished Successfully!"
