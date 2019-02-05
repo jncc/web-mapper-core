452022a990ac0f7c2fe32d574ec42b9b12e7f782
Simon Annetts <simonannetts@esdm.co.uk>
2019-02-05 12:36:42 +0000

    Create vscode task to deploy from our test server to their live server

---
+++ web-api/.vscode/deploy_to_live.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+project="jncc-web-api"
+host="esdm-xen1-04.esdm.co.uk"
+livehost="ec2-63-34-227-23.eu-west-1.compute.amazonaws.com"
+user="esdm"
+remotepath="/home/esdm/dotnet-published"
+
+echo "Running Deploy to Live Task on server...."
+ssh $user@$host "mkdir $remotepath 2>/dev/null; rsync -e ssh -zauvE --delete --progress $remotepath/$project $user@$livehost:$remotepath"
+[ $? -ne 0 ] && echo "Deploy to Live Task Failed!" && exit 1
