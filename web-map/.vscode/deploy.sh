ac47d2fbe2ea252d5bac69615dc16e103fa1b2d7
Simon Annetts <simonannetts@esdm.co.uk>
2019-02-12 14:46:42 +0000

    Make sure the correct configuration is used on the correct staging server

--- web-map/.vscode/deploy.sh
+++ web-map/.vscode/deploy.sh
@@ -8,10 +8,19 @@ remotepath="/home/esdm/web-published"
 echo "Building solution in Production mode..."
 ng build --prod
 [ $? -ne 0 ] && echo "Build/Publish Task Failed!" && exit 1
+
+echo "Cleaning Remote Deployment Directory..."
 ssh $user@$host "cd $remotepath/$project && rm -Rf *"
 [ $? -ne 0 ] && echo "Deploy Clean Failed!" && exit 1
+
+echo "Deploying Files to Remote Server..."
 sftp "$user@$host:$remotepath/$project" <<EOF
 mput -r dist/web-map/*
 EOF
 [ $? -ne 0 ] && echo "Deploy Transfer Failed!" && exit 1
+
+echo "Installing Correct Configuration..."
+ssh $user@$host "cd $remotepath/$project/assets/config && mv -f config.prod.esdm.json config.prod.json"
+[ $? -ne 0 ] && echo "Installing Configuration Failed!" && exit 1
+
 echo "Deploy Finished Successfully!"
--- web-map/.vscode/deploy_to_live.sh
+++ web-map/.vscode/deploy_to_live.sh
@@ -9,3 +9,8 @@ remotepath="/home/esdm/web-published"
 echo "Running Deploy to Live Task on server...."
 ssh $user@$host "mkdir $remotepath 2>/dev/null; rsync -e ssh -zauvE --delete --progress $remotepath/$project $user@$livehost:$remotepath"
 [ $? -ne 0 ] && echo "Deploy to Live Task Failed!" && exit 1
+
+echo "Installing Correct Configuration on Live Server..."
+rc="ssh $user@$livehost \"cd $remotepath/$project/assets/config && mv -f config.prod.jncc.json config.prod.json\""
+ssh $user@$host "$rc"
+[ $? -ne 0 ] && echo "Installing Configuration Failed!" && exit 1
