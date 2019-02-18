1e7c2ba46dd99f4669b829d8f953b0a93b6bff8f
andyb-esdm <andybrewer@esdm.co.uk>
2019-02-18 19:17:08 +0000

    legend component

--- web-map/src/app/app.component.ts
+++ web-map/src/app/app.component.ts
@@ -9,33 +9,10 @@ import { Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
 
-  show = false;
-  legend: { name: string, legendUrl: string };
-
-  constructor(private mapService: MapService) {
+  constructor() {
     const converter = new XmlConverter();
     converter.convert();
-
-    this.showLegendSubscription = this.mapService.showLegendSubject.subscribe(legend => {
-      this.showLegend(legend);
-    });
-  }
-
-  showLegend(legend: { name: string, legendUrl: string }) {
-    if (legend) {
-      this.show = true;
-      this.legend = legend;
-    } else {
-      this.show = false;
-      this.legend = null;
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.showLegendSubscription) {
-      this.showLegendSubscription.unsubscribe();
-    }
   }
 }
--- web-map/src/app/legend/legend.component.ts
+++ web-map/src/app/legend/legend.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 import { MapService } from '../map.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-legend',
   templateUrl: './legend.component.html',
-  styleUrls: ['./legend.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./legend.component.scss']
 })
-export class LegendComponent implements OnInit, OnChanges {
-
-  @Input() legend: { name: string, legendUrl: string } = { name: '', legendUrl: ''};
+export class LegendComponent implements OnInit, OnDestroy {
 
   loading = true;
+  show = false;
+  legend: { name: string, legendUrl: string };
+  showLegendSubscription: Subscription;
 
   constructor(private mapService: MapService) { }
 
   ngOnInit() {
+    this.showLegendSubscription = this.mapService.showLegendSubject.subscribe(legend => {
+      this.showLegend(legend);
+    });
   }
 
-  ngOnChanges(changes: SimpleChanges) {
+  private testngOnChanges(changes: SimpleChanges) {
     console.log(changes.legend);
     if (changes.legend.previousValue) {
       if (changes.legend.previousValue.legendUrl !== changes.legend.currentValue.legendUrl) {
@@ -26,14 +30,37 @@ export class LegendComponent implements OnInit, OnChanges {
       }
     }
   }
-
-
   onHideLegend() {
     this.mapService.hideLegend();
   }
 
-  onLoaded() {
+  showLegend(legend: { name: string, legendUrl: string }) {
+    if (legend) {
+      this.show = true;
+      // if browser caches image then img load event isn't raised so test for this
+      // so spinner displays appropriately
+      // TODO: test this will always work
+      if (this.legend) {
+        if (this.legend.legendUrl !== legend.legendUrl) {
+          this.loading = true;
+        }
+      }
+      this.legend = legend;
+    } else {
+      this.show = false;
+      this.legend = null;
+    }
+    console.log(this.legend);
+  }
+
+  onLegendImageLoaded() {
     this.loading = false;
+  }
+
+  ngOnDestroy() {
+    if (this.showLegendSubscription) {
+      this.showLegendSubscription.unsubscribe();
+    }
   }
 
 }
