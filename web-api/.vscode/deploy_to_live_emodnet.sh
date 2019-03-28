ac9adf5dcbb392afbcb5642e8c64479a44e8282a
Simon Annetts <simonannetts@esdm.co.uk>
2019-03-28 16:12:59 +0000

    Add deploy to MPA

---
+++ web-api/.vscode/deploy_to_live_emodnet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+project="jncc-web-api"
+host="esdm-xen1-04.esdm.co.uk"
+livehost="ec2-63-34-227-23.eu-west-1.compute.amazonaws.com"
+user="esdm"
+remotepath="/home/esdm/dotnet-published"
+
+echo "Running Deploy to Live Task on EMODnet server...."
+ssh $user@$host "mkdir $remotepath 2>/dev/null; rsync -e ssh -zauvE --delete --progress --exclude=appsettings.json $remotepath/$project $user@$livehost:$remotepath"
+[ $? -ne 0 ] && echo "Deploy to Live Task Failed!" && exit 1
+
+echo "Installing Correct Configuration on EMODnet Live Server and Restarting Process..."
+rc="ssh $user@$livehost \"cd $remotepath/$project && cp -f appsettings.emod.json appsettings.json && cp -f appsettings.emod.json appsettings.Development.json && rm -f appsettings.mpa.json && kill \\\$(pgrep -f -o '$project')\""
+echo $rc
+ssh $user@$host "$rc"
+[ $? -ne 0 ] && echo "Installing Configuration and Restarting Failed!" && exit 1
--- web-api/.vscode/deploy_to_live_mpa.sh
+++ web-api/.vscode/deploy_to_live_mpa.sh
@@ -2,16 +2,16 @@
 
 project="jncc-web-api"
 host="esdm-xen1-04.esdm.co.uk"
-livehost="ec2-63-34-227-23.eu-west-1.compute.amazonaws.com"
+livehost="api.mpa.jncc.gov.uk"
 user="esdm"
 remotepath="/home/esdm/dotnet-published"
 
-echo "Running Deploy to Live Task on server...."
+echo "Running Deploy to Live Task on MPA server...."
 ssh $user@$host "mkdir $remotepath 2>/dev/null; rsync -e ssh -zauvE --delete --progress --exclude=appsettings.json $remotepath/$project $user@$livehost:$remotepath"
 [ $? -ne 0 ] && echo "Deploy to Live Task Failed!" && exit 1
 
-echo "Installing Correct Configuration on Live Server and Restarting Process..."
-rc="ssh $user@$livehost \"cd $remotepath/$project && cp -f appsettings.jncc.json appsettings.json && cp -f appsettings.jncc.json appsettings.Development.json && kill \\\$(pgrep -f -o '$project')\""
+echo "Installing Correct Configuration on MPA Live Server and Restarting Process..."
+rc="ssh $user@$livehost \"cd $remotepath/$project && cp -f appsettings.mpa.json appsettings.json && cp -f appsettings.mpa.json appsettings.Development.json && rm -f appsettings.jncc.json && kill \\\$(pgrep -f -o '$project')\""
 echo $rc
 ssh $user@$host "$rc"
 [ $? -ne 0 ] && echo "Installing Configuration and Restarting Failed!" && exit 1
