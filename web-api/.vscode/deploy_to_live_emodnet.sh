67da505a53698e22a2be109a81c6688197fefc1f
Simon Annetts <simonannetts@esdm.co.uk>
2019-03-28 16:29:13 +0000

    Fix deploy scripts appsettings choice

--- web-api/.vscode/deploy_to_live_emodnet.sh
+++ web-api/.vscode/deploy_to_live_emodnet.sh
@@ -7,11 +7,11 @@ user="esdm"
 remotepath="/home/esdm/dotnet-published"
 
 echo "Running Deploy to Live Task on EMODnet server...."
-ssh $user@$host "mkdir $remotepath 2>/dev/null; rsync -e ssh -zauvE --delete --progress --exclude=appsettings.json $remotepath/$project $user@$livehost:$remotepath"
+ssh $user@$host "mkdir $remotepath 2>/dev/null; rsync -e ssh -zauvE --delete --progress --exclude=appsettings.json  --exclude=appsettings.mpa.json --exclude=appsettings.esdm.json $remotepath/$project $user@$livehost:$remotepath"
 [ $? -ne 0 ] && echo "Deploy to Live Task Failed!" && exit 1
 
 echo "Installing Correct Configuration on EMODnet Live Server and Restarting Process..."
-rc="ssh $user@$livehost \"cd $remotepath/$project && cp -f appsettings.emod.json appsettings.json && cp -f appsettings.emod.json appsettings.Development.json && rm -f appsettings.mpa.json && kill \\\$(pgrep -f -o '$project')\""
+rc="ssh $user@$livehost \"cd $remotepath/$project && cp -f appsettings.emod.json appsettings.json && cp -f appsettings.emod.json appsettings.Development.json && kill \\\$(pgrep -f -o '$project')\""
 echo $rc
 ssh $user@$host "$rc"
 [ $? -ne 0 ] && echo "Installing Configuration and Restarting Failed!" && exit 1
