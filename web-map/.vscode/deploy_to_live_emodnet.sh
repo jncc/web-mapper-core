a7678db4e693761b93c8310ec9982444ce8c2d75
Simon Annetts <simonannetts@esdm.co.uk>
2019-03-28 16:39:19 +0000

    Add deploy script for MPA

--- web-map/.vscode/deploy_to_live_emodnet.sh
+++ web-map/.vscode/deploy_to_live_emodnet.sh
@@ -6,11 +6,11 @@ livehost="ec2-63-34-227-23.eu-west-1.compute.amazonaws.com"
 user="esdm"
 remotepath="/home/esdm/web-published"
 
-echo "Running Deploy to Live Task on server...."
+echo "Running Deploy to EMODnet Live Task on server...."
 ssh $user@$host "mkdir $remotepath 2>/dev/null; rsync -e ssh -zauvE --delete --progress $remotepath/$project $user@$livehost:$remotepath"
 [ $? -ne 0 ] && echo "Deploy to Live Task Failed!" && exit 1
 
-echo "Installing Correct Configuration on Live Server..."
-rc="ssh $user@$livehost \"cd $remotepath/$project/assets/config && cp -f config.prod.jncc.json config.prod.json\""
+echo "Installing Correct Configuration on EMODnet Live Server..."
+rc="ssh $user@$livehost \"cd $remotepath/$project/assets/config && cp -f config.prod.emod.json config.prod.json\""
 ssh $user@$host "$rc"
 [ $? -ne 0 ] && echo "Installing Configuration Failed!" && exit 1
---
+++ web-map/.vscode/deploy_to_live_mpa.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+project="jncc-web-map"
+host="esdm-xen1-04.esdm.co.uk"
+livehost="mapper.mpa.jncc.gov.uk"
+user="esdm"
+remotepath="/home/esdm/web-published"
+
+echo "Running Deploy to MPA Live Task on server...."
+ssh $user@$host "mkdir $remotepath 2>/dev/null; rsync -e ssh -zauvE --delete --progress $remotepath/$project $user@$livehost:$remotepath"
+[ $? -ne 0 ] && echo "Deploy to Live Task Failed!" && exit 1
+
+echo "Installing Correct Configuration on MPA Live Server..."
+rc="ssh $user@$livehost \"cd $remotepath/$project/assets/config && cp -f config.prod.mpa.json config.prod.json\""
+ssh $user@$host "$rc"
+[ $? -ne 0 ] && echo "Installing Configuration Failed!" && exit 1
